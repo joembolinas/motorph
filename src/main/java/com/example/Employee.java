787d5ab439733afcb6eeb4c3a7b2f2b721e5b9cd
Joem Bolinas <lr.jbolinas@mmdc.mcl.edu.ph>
2025-02-15 06:40:30 +0800

    week 6

---
+++ src/main/java/com/example/Employee.java
@@ -0,0 +1,48 @@
+package com.example;
+
+public class Employee {
+    
+    public static void main(String[] args) {
+
+
+        int employeeNumber = 10001;
+        String lastName = "Garcia";
+        String firstName = "Manuel III";
+        String birthday = "10/11/1983";
+        String address = "Valero Carpark Building Valero Street 1227, Makati City";
+        String phoneNumber = "966-860-270";
+        double basicSalary = 90000.00;
+        double hourlyRate = 535.71;
+
+        System.out.println("Employee Number: " + employeeNumber);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Birthday: " + birthday);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Basic Salary: " + basicSalary);
+        System.out.println("Hourly Rate: " + hourlyRate);
+
+
+        // Display employee details
+        System.out.println("Employee Number: " + employeeNumber);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Birthday: " + birthday);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Basic Salary: " + basicSalary);
+        System.out.println("Hourly Rate: " + hourlyRate);
+
+        System.out.println("Employee Number: " + employeeNumber);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Birthday: " + birthday);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Basic Salary: " + basicSalary);
+        System.out.println("Hourly Rate: " + hourlyRate);
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/example/Employee.java
@@ -0,0 +1,46 @@
+package com.example;
+
+public class Employee {
+    
+    public static void main(String[] args) {
+        int employeeNumber = 10001;
+        String lastName = "Garcia";
+        String firstName = "Manuel III";
+        String birthday = "10/11/1983";
+        String address = "Valero Carpark Building, Valero Street 1227, Makati City";
+        String phoneNumber = "966-860-270";
+        String sssNumber = "44-4506057-3";
+        String philhealthNumber = "820126853951";
+        String tinNumber = "442-605-657-000";
+        String pagibigNumber = "691295330870";
+        String status = "Regular";
+        String position = "Chief Executive Officer";
+        String immediateSupervisor = "N/A";
+        int basicSalary = 90000;
+        int riceSubsidy = 1500;
+        int phoneAllowance = 2000;
+        int clothingAllowance = 1000;
+        int grossSemiMonthlyRate = 45000;
+        double hourlyRate = 535.71;
+
+        System.out.println("Employee Number: " + employeeNumber);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Birthday: " + birthday);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("SSS Number: " + sssNumber);
+        System.out.println("Philhealth Number: " + philhealthNumber);
+        System.out.println("TIN Number: " + tinNumber);
+        System.out.println("Pagibig Number: " + pagibigNumber);
+        System.out.println("Status: " + status);
+        System.out.println("Position: " + position);
+        System.out.println("Immediate Supervisor: " + immediateSupervisor);
+        System.out.println("Basic Salary: " + basicSalary);
+        System.out.println("Rice Subsidy: " + riceSubsidy);
+        System.out.println("Phone Allowance: " + phoneAllowance);
+        System.out.println("Clothing Allowance: " + clothingAllowance);
+        System.out.println("Gross Semi-Monthly Rate: " + grossSemiMonthlyRate);
+        System.out.println("Hourly Rate: " + hourlyRate);
+    }
+}
